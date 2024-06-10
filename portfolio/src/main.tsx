05159f733b57803da85864e56cec60e47678d801
sholanke precious <sholankeprecious3@gmail.com>
2024-06-10 10:42:04 +0100

    added bootstrap

--- portfolio/src/main.tsx
+++ portfolio/src/main.tsx
@@ -1,8 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
 import "./App.css"; // Import your custom CSS file if needed
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
